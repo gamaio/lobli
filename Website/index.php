d86eb10d1f07340cd7e3497a3554279ff273de6a
alopexc0de <c0de@unps.us>
2014-06-26 20:38:35 -0400

    Move database connection to independant file

--- Website/index.php
+++ Website/index.php
@@ -22,8 +22,7 @@
       exit 13 - Shortener About redirection
   */
 
-  $shortdb = new mysqli('localhost', 'short', 'password', 'short'); // Connect to link shortener DB
-  if($shortdb->connect_errno > 0) die('Unable to connect to database [' . $shortdb->connect_error . '] - Check dbsettings.php');
+  require('Include/PHP/db.php');
 
   // This has been depreciated. Still here for backwards compatibility with existing links
   if(!empty($_GET['l'])){
